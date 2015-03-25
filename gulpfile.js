8f8142cb326008508791ba0ded91a2d5e84f356b
Matthew Hartman <hello@matthewhartman.com.au>
2015-03-25 18:47:37 +1100

    Base Updates - v1.8.6 - Moved .no-padding and .no-margin helpers from _grid.less into _helpers.less - Moved .no-padding and .no-margin helpers from _grid.scss into _helpers.scss - Updated style.less to include _helpers.less after _grid.less - Updated style.less to include _helpers.scss after _grid.scss - Added gulp file for LESS and SASS compiling :)

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
+    less = require('gulp-less'),
+    path = require('path'),
+    notify = require('gulp-notify');
+
+gulp.task('sass', function () {
+  gulp.src('scss/style.scss')
+    .pipe(sass({
+      errLogToConsole: true,
+      paths: [ path.join(__dirname, 'scss', 'includes') ]
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(notify("SASS Compiled Successfully :)"));
+});
+
+gulp.task('less', function () {
+  gulp.src('less/style.less')
+    .pipe(less({ paths: [ path.join(__dirname, 'less', 'includes') ]
+  })
+  .on('error', function(err) {
+    this.emit('end');
+  }))
+  .on("error", notify.onError(function(error) {
+    return "Failed to Compile LESS: " + error.message;
+  }))
+  .pipe(gulp.dest('./'))
+  .pipe(notify("LESS Compiled Successfully :)"));
+});
+
+gulp.task('watch', function () {
+   gulp.watch('scss/**/*', ['sass']),
+   gulp.watch('less/**/*', ['less']);
+});
+
+gulp.task('default', ['watch']);
