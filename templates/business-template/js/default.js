580c9d4dfb0517307f44f3014d5ae667fe64fb2c
Matthew Hartman <hello@matthewhartman.com.au>
2017-04-08 17:42:20 +1000

    Added template demos

---
+++ templates/business-template/js/default.js
@@ -0,0 +1,1 @@
+var html=document.querySelector("html");html.className=html.className.replace(/\bno-js\b/,"js");var wow=new WOW({boxClass:"animate-block",animateClass:"active",offset:1,mobile:!0,live:!0});wow.init();
---
+++ templates/restaurant-template/js/default.js
@@ -0,0 +1,1 @@
+var wow=new WOW({boxClass:"content-block",animateClass:"active",offset:1,mobile:!0,live:!0});wow.init(),WebFont.load({google:{families:["Open Sans:400,700","Yeseva One:400"]}});
