e4359229089161328991f6d4b13709d3ddfcb1fc
Matthew Hartman <matthewhartmanau@gmail.com>
2018-03-24 23:29:20 +1100

    Added LESS

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 'use strict';
 
-let browserSync = require('browser-sync').create()
-let path = require('path')
-let del = require('del')
-let gulp = require('gulp')
-let autoprefixer = require('gulp-autoprefixer')
-let notify = require('gulp-notify')
-let sass = require('gulp-sass')
-let sourcemaps = require('gulp-sourcemaps')
+let browserSync = require('browser-sync').create();
+let path = require('path');
+let del = require('del');
+let gulp = require('gulp');
+let autoprefixer = require('gulp-autoprefixer');
+let notify = require('gulp-notify');
+let sass = require('gulp-sass');
+let sourcemaps = require('gulp-sourcemaps');
+let less = require('gulp-less');
 let runSequence = require('run-sequence');
 
 // Task to compile SCSS
@@ -19,7 +20,7 @@ gulp.task('sass', () => {
       errLogToConsole: false,
       paths: [path.join(__dirname, 'scss', 'includes')]
     })
-      .on("error", notify.onError((error) => {
+      .on("error", notify.onError(error => {
         return "Failed to Compile SCSS: " + error.message;
       })))
     .pipe(autoprefixer())
@@ -28,6 +29,26 @@ gulp.task('sass', () => {
     .pipe(browserSync.stream())
     .pipe(notify({
       message: "SCSS Compiled Successfully :)",
+      onLast: true
+    }));
+});
+
+// Task to compile LESS
+gulp.task('less', () => {
+  return gulp.src('./less/styles.less')
+    .pipe(sourcemaps.init())
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    })
+    .on("error", notify.onError(error => {
+      return "Failed to Compile LESS: " + error.message;
+    })))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css/'))
+    .pipe(browserSync.stream())
+    .pipe(notify({
+      message: "LESS Compiled Successfully :)",
       onLast: true
     }));
 });
@@ -44,6 +65,7 @@ gulp.task('browserSync', () => {
 // Gulp Watch Task
 gulp.task('watch', ['browserSync'], () => {
   gulp.watch('./scss/**/*', ['sass']);
+  gulp.watch('./less/**/*', ['less']);
   gulp.watch('./**/*.html').on('change', browserSync.reload);
 });
 
