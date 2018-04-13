d2eb111f2d9ee55b10a70c08cd3b0aeae857c4a6
Matthew Hartman <matthewhartmanau@gmail.com>
2018-04-14 09:27:27 +1000

    Updated business template

--- templates/business-template/js/default.js
+++
@@ -1,1 +0,0 @@
-var html=document.querySelector("html");html.className=html.className.replace(/\bno-js\b/,"js");var wow=new WOW({boxClass:"animate-block",animateClass:"active",offset:1,mobile:!0,live:!0});wow.init();
