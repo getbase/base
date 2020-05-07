86d04e0176c3ec4985dd057831944fef999cdf76
Matthew Hartman <matthewhartmanau@gmail.com>
2020-05-07 22:45:12 +1000

    Fixed layout spacing. Added new beauty template

--- dist/default.fe761c5a.js
+++ dist/default.fe761c5a.js
@@ -175,7 +175,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55676" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49800" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
